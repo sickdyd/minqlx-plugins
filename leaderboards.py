c7fd39753d251c679475beac8e69d74218373359
sickdyd <roberto.reale.ja@gmail.com>
2025-02-23 11:02:34 +0900

    Updated

--- leaderboards.py
+++ leaderboards.py
@@ -5,30 +5,57 @@ import requests
 from datetime import datetime, timedelta
 
 RELEVANT_WEAPONS = ["lightning", "grenade", "rocket", "railgun", "plasma", "machinegun", "hmg", "shotgun"]
-SNIPER_MEDALS = ["accuracy", "headshot", "impressive",]
-ATTACKER_MEDALS = ["excellent", "firstfrag", "midair", "revenge"]
-LEADERBOARS_HOST = "http://localhost:3000/api/v1"
+# SNIPER_MEDALS = ["accuracy", "headshot", "impressive",]
+# ATTACKER_MEDALS = ["excellent", "firstfrag", "midair", "revenge"]
+RELEVANT_MEDALS = ["accuracy", "impressive", "excellent", "midair",]
+AVAILABLE_LEADERBOARDS = ["accuracy", "best", "damage", "damage_taken", "kills", "deaths", "medals", "wins", "losses"]
+AVAILABLE_TIME_FILTERS = ["all_time", "monthly", "weekly", "daily"]
+LEADERBOARS_HOST = "http://qlove_api:3000/api/v1"
 
 class leaderboards(minqlx.Plugin):
     def __init__(self):
         self.leaderboards_host = self.get_cvar("qlx_qloveLeaderboardsHost") or LEADERBOARS_HOST
-        self.logger.info(f"Leaderboards host: {self.leaderboards_host}")
 
         self.add_command("lb", self.cmd_leaderboard, priority=minqlx.PRI_HIGH, usage = "!lb <type> <timeframe>")
 
     def cmd_leaderboard(self, player, msg, channel):
         lb_type = msg[1].lower()
-        time_filter = "month"
+        time_filter = msg[2].lower() if len(msg) > 2 else "day"
+
+        if lb_type == "help":
+            return player.tell(f"Usage: !lb <type> <timeframe>\nAvailable leaderboards: {', '.join(AVAILABLE_LEADERBOARDS)}\nAvailable timeframes: {', '.join(AVAILABLE_TIME_FILTERS)}")
+
+        if not self.is_valid_time_filter(time_filter):
+            return player.tell(f"Invalid time filter: {time_filter}. Available time filters: {', '.join(AVAILABLE_TIME_FILTERS)}")
+
+        if not self.is_valid_leaderboard(lb_type):
+            return player.tell(f"Invalid leaderboard type: {lb_type}. Available leaderboards: {', '.join(AVAILABLE_LEADERBOARDS)}")
+
+        weapons = ",".join(RELEVANT_WEAPONS)
+        medals = ",".join(RELEVANT_MEDALS)
+
         self.logger.info(f"Leaderboard type: {lb_type}, time_filter: {time_filter}")
+        url = f"{self.leaderboards_host}/leaderboards/{lb_type}?time_filter={time_filter}&weapons={weapons}&medals={medals}&formatted_table=true"
+        self.fetch(url, self.handle_leaderboard_request, player)
 
-        if lb_type == "accuracy":
-            weapons = ",".join(RELEVANT_WEAPONS)
-            url = f"{self.leaderboards_host}/leaderboards/{lb_type}?time_filter={time_filter}&weapons={weapons}&formatted_table=true"
-            self.fetch(url, self.handle_accuracy_leaderboard, player, lb_type, time_filter)
+    def is_valid_leaderboard(self, lb_type):
+        return lb_type in AVAILABLE_LEADERBOARDS
 
-    def handle_accuracy_leaderboard(self, data, player, lb_type, time_filter):
+    def is_valid_time_filter(self, time_filter):
+        return time_filter in AVAILABLE_TIME_FILTERS
+
+    def handle_leaderboard_request(self, data, player):
         table_data = data.get("data", [])
         self.send_multiline_message(player, table_data)
+
+    def handle_team_switch(self, player):
+        url = f"{self.leaderboards_host}/leaderboards/{lb_type}?time_filter={time_filter}&weapons={weapons}&medals={medals}&formatted_table=true"
+        self.fetch(url, self.show_best_players, player)
+
+    def show_best_players(self, data, player):
+        table_data = data.get("data", [])
+        
+        player.center_print(f"\n\nToday's ^3BEST^7 players:\n\n{top_names}")
 
     # Helper functions
 
