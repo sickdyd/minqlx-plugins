ea0031d99b4a3b487f0121ad5480db8b0e6e3a67
sickdyd <roberto.reale.ja@gmail.com>
2025-03-15 12:44:57 +0900

    Various fixes

--- leaderboards.py
+++ leaderboards.py
@@ -5,41 +5,90 @@ import requests
 from datetime import datetime, timedelta
 
 RELEVANT_WEAPONS = ["lightning", "grenade", "rocket", "railgun", "plasma", "machinegun", "hmg", "shotgun"]
-# SNIPER_MEDALS = ["accuracy", "headshot", "impressive",]
-# ATTACKER_MEDALS = ["excellent", "firstfrag", "midair", "revenge"]
-RELEVANT_MEDALS = ["accuracy", "impressive", "excellent", "midair",]
-AVAILABLE_LEADERBOARDS = ["accuracy", "best", "damage", "damage_taken", "kills", "deaths", "medals", "wins", "losses"]
-AVAILABLE_TIME_FILTERS = ["all_time", "monthly", "weekly", "daily"]
+SNIPER_MEDALS = ["accuracy", "headshot", "impressive",]
+ATTACKER_MEDALS = ["excellent", "firstfrag", "midair", "revenge"]
+AVAILABLE_LEADERBOARDS = ["accuracy", "best", "damage", "damage_taken", "kills", "deaths", "snipers", "attackers", "wins", "losses"]
+AVAILABLE_TIME_FILTERS = ["day", "week", "month", "year", "all_time"]
+DEFAULT_LIMIT = 10
+DEFAULT_TIME_FILTER = "day"
 LEADERBOARS_HOST = "http://qlove_api:3000/api/v1"
+HIGHLITHED_LIST_ENTRIES_SEPARATOR = "^7, ^6"
 
 class leaderboards(minqlx.Plugin):
     def __init__(self):
         self.leaderboards_host = self.get_cvar("qlx_qloveLeaderboardsHost") or LEADERBOARS_HOST
 
-        self.add_command("lb", self.cmd_leaderboard, priority=minqlx.PRI_HIGH, usage = "!lb <type> <timeframe>")
+        self.add_command("lb", self.cmd_leaderboard, priority=minqlx.PRI_HIGH, usage = "!lb <^6type^7> <^6timeframe^7>")
+        self.add_command("stats", self.cmd_stats, priority=minqlx.PRI_HIGH, usage = "!stats <^6timeframe^7>")
+        self.add_hook("team_switch", self.handle_team_switch)
 
     def cmd_leaderboard(self, player, msg, channel):
         lb_type = msg[1].lower()
-        time_filter = msg[2].lower() if len(msg) > 2 else "day"
+        time_filter = msg[2].lower() if len(msg) > 2 else DEFAULT_TIME_FILTER
 
         if lb_type == "help":
-            return player.tell(f"Usage: !lb <type> <timeframe>\nAvailable leaderboards: {', '.join(AVAILABLE_LEADERBOARDS)}\nAvailable timeframes: {', '.join(AVAILABLE_TIME_FILTERS)}")
-
-        if not self.is_valid_time_filter(time_filter):
-            return player.tell(f"Invalid time filter: {time_filter}. Available time filters: {', '.join(AVAILABLE_TIME_FILTERS)}")
+            player.tell(f"Usage: !lb <^6type^7> <^2timeframe^7> (defaults to 'day' timeframe)")
+            player.tell(f"Available leaderboards: ^6all^7, ^6{HIGHLITHED_LIST_ENTRIES_SEPARATOR.join(AVAILABLE_LEADERBOARDS)}")
+            player.tell(f"Available timeframes: ^6{HIGHLITHED_LIST_ENTRIES_SEPARATOR.join(AVAILABLE_TIME_FILTERS)}")
+            return
 
         if not self.is_valid_leaderboard(lb_type):
-            return player.tell(f"Invalid leaderboard type: {lb_type}. Available leaderboards: {', '.join(AVAILABLE_LEADERBOARDS)}")
+            player.tell(f"Invalid leaderboard type: ^2{lb_type}^7.")
+            player.tell(f"Available leaderboards: ^6all^7, ^6{HIGHLITHED_LIST_ENTRIES_SEPARATOR.join(AVAILABLE_LEADERBOARDS)}")
+            return
+
+        if not self.is_valid_time_filter(time_filter):
+            player.tell(f"Invalid time filter: ^2{time_filter}^7.")
+            player.tell(f"Available time filters: ^6{HIGHLITHED_LIST_ENTRIES_SEPARATOR.join(AVAILABLE_TIME_FILTERS)}")
+            return
 
         weapons = ",".join(RELEVANT_WEAPONS)
-        medals = ",".join(RELEVANT_MEDALS)
+        medals = ""
+
+        if msg[1] == "snipers":
+            lb_type = "medals"
+            medals = ",".join(SNIPER_MEDALS)
+        elif msg[1] == "attackers":
+            lb_type = "medals"
+            medals = ",".join(ATTACKER_MEDALS)
 
         self.logger.info(f"Leaderboard type: {lb_type}, time_filter: {time_filter}")
-        url = f"{self.leaderboards_host}/leaderboards/{lb_type}?time_filter={time_filter}&weapons={weapons}&medals={medals}&formatted_table=true"
+        if lb_type == "all":
+            self.request_all_leaderboards(player, time_filter, weapons, medals)
+        else:
+            self.request_leaderboard(player, lb_type, time_filter, weapons, medals)
+
+    def cmd_stats(self, player, msg, channel):
+        time_filter = msg[1].lower() if len(msg) > 1 else DEFAULT_TIME_FILTER
+
+        if not self.is_valid_time_filter(time_filter):
+            player.tell(f"Invalid time filter: ^2{time_filter}^7.")
+            player.tell(f"Available time filters: ^6{HIGHLITHED_LIST_ENTRIES_SEPARATOR.join(AVAILABLE_TIME_FILTERS)}")
+            return
+
+        weapons = ",".join(RELEVANT_WEAPONS)
+
+        self.request_stats(player, time_filter, weapons)
+
+    @minqlx.thread
+    def request_all_leaderboards(self, player, time_filter, weapons, medals):
+        for lb in AVAILABLE_LEADERBOARDS:
+            if lb in ["snipers", "attackers"]:
+                medals = ",".join(SNIPER_MEDALS) if lb == "snipers" else ",".join(ATTACKER_MEDALS)
+                lb = "medals"
+            time.sleep(0.1)
+            self.request_leaderboard(player, lb, time_filter, weapons, medals)
+
+    def request_leaderboard(self, player, lb_type, time_filter, weapons, medals):
+        url = self.request_url(lb_type, time_filter, weapons, medals)
         self.fetch(url, self.handle_leaderboard_request, player)
 
+    def request_stats(self, player, time_filter, weapons):
+        url = f"{self.leaderboards_host}/stats?time_filter={time_filter}&weapons={weapons}&steam_id={player.steam_id}"
+        self.fetch(url, self.handle_stats_request, player)
+
     def is_valid_leaderboard(self, lb_type):
-        return lb_type in AVAILABLE_LEADERBOARDS
+        return lb_type in AVAILABLE_LEADERBOARDS + ["all"]
 
     def is_valid_time_filter(self, time_filter):
         return time_filter in AVAILABLE_TIME_FILTERS
@@ -48,16 +97,74 @@ class leaderboards(minqlx.Plugin):
         table_data = data.get("data", [])
         self.send_multiline_message(player, table_data)
 
-    def handle_team_switch(self, player):
-        url = f"{self.leaderboards_host}/leaderboards/{lb_type}?time_filter={time_filter}&weapons={weapons}&medals={medals}&formatted_table=true"
+    def handle_stats_request(self, data, player):
+        stats_data = data.get("data", [])
+
+        if not stats_data:
+            player.tell("Failed to fetch data.")
+            return
+
+        stats = stats_data[0]
+
+        stats_message = self.colorize_stats(stats, player)
+        player.tell(f"{stats_message}")
+
+    def colorize_stats(self, stats, player):
+        player.tell(f"Stats for {stats}:")
+
+        def get_color(value):
+            if value is None or value == "":
+                return "^7-"  # Default color for missing stats
+            try:
+                value = float(value)
+            except ValueError:
+                return "^7-"  # If conversion fails, treat as missing data
+
+            if value > 35:
+                return f"^2{value}"  # Green
+            elif value >= 30:
+                return f"^3{value}"  # Yellow
+            else:
+                return f"^1{value}"  # Red
+
+        # Extract stats and apply coloring
+        stat_names = ["avg", "lg", "gl", "rl", "rg", "pg", "mg", "hmg", "sg"]
+        colored_stats = [f"^7{stat}: {get_color(stats.get(stat))}" for stat in stat_names]
+
+        return ", ".join(colored_stats)  # Join stats with commas for readability
+
+    def request_url(self, lb_type, time_filter, weapons, medals, formatted_table="true", limit=DEFAULT_LIMIT):
+        return f"{self.leaderboards_host}/leaderboards/{lb_type}?time_filter={time_filter}&weapons={weapons}&medals={medals}&formatted_table={formatted_table}&limit={DEFAULT_LIMIT}"
+
+    def handle_team_switch(self, player, old_team, new_team):
+        url = self.request_url("best", "day", "", "", "false")
         self.fetch(url, self.show_best_players, player)
 
     def show_best_players(self, data, player):
-        table_data = data.get("data", [])
-        
+        if not data:
+            player.tell("Failed to fetch data.")
+            return
+
+        top_names = ""
+        for i, player_data in enumerate(data.get("data", [])):
+            player_name = player_data.get("name", "Unknown")
+            player_name = self.strip_formatting(player_name)
+            player_name = self.truncate(player_name, 15)
+            strength = player_data.get("strength")
+
+            top_names += f"{i + 1}. {player_name} (score {strength})\n"
+
+        time.sleep(4)
+
         player.center_print(f"\n\nToday's ^3BEST^7 players:\n\n{top_names}")
 
     # Helper functions
+
+    def strip_formatting(self, text):
+        return re.sub(r"\^.", "", text)
+
+    def truncate(self, text, length):
+        return text[:length] if len(text) > length else text
 
     @minqlx.thread
     def fetch(self, endpoint, callback, *args, **kwargs):
@@ -80,3 +187,6 @@ class leaderboards(minqlx.Plugin):
         for line in message.splitlines():
             time.sleep(0.01)
             player.tell(line)
+        for _ in range(5):
+            player.tell("")
+        player.tell("Check the console to see the leaderboard!")
